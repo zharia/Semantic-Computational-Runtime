b1b7acd74b71924223082d2efe033372019e17cc
Craig Peacock <craig@bytemoney.co.za>
2026-09-15 21:51:17 +0200

    Removed incorrect graph files

--- lib/203_Graph/IR/mlir/build/lib/CMakeFiles/install-SCRdialect.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-SCRdialect"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-SCRdialect.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
